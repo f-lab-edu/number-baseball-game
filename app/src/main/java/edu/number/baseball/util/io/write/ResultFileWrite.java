4abfe143c28055f91802b8e5fac0cdca4bb49618
Hero <81640511+icraft2170@users.noreply.github.com>
2022-07-28 06:40:41 +0900

    other: #14 OS별 파일저장 경로 대응. - File.seperator 설정

--- app/src/main/java/edu/number/baseball/util/io/write/ResultFileWrite.java
+++ app/src/main/java/edu/number/baseball/util/io/write/ResultFileWrite.java
@@ -1,24 +1,41 @@
 package edu.number.baseball.util.io.write;
 
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 import static java.nio.file.StandardOpenOption.*;
 
 
 public abstract class ResultFileWrite {
-    public static final String RESULT_FILE_PATH = "app/src/main/resources/result";
 
     public static void resultWrite(String result) {
+        String resultFilePath = getResultFilePath();;
         try {
-            Path filePath = Paths.get(RESULT_FILE_PATH);
+            Path filePath = Paths.get(resultFilePath);
             byte[] resultBuffer = result.getBytes();
             Files.write(filePath, resultBuffer, CREATE ,APPEND);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private static String getResultFilePath() {
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.contains("mac")) {
+            String resultPath = "./app/src/main/resources/result";
+            resultPath.replaceAll("/", File.separator);
+            return  resultPath;
+        } else if (osName.contains("window")){
+            String resultPath = "./app/src/main/resources/result";
+            resultPath.replaceAll("/", File.separator);
+            return resultPath;
+        }
+        return "app/src/main/resources/result";
+    }
 }
+
